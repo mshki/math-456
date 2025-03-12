1ff0ac615a6d80cba18a4ecea09fd3250694006e
Archimedes Li <archimedes.li@outlook.com>
2025-03-11 20:57:56 -0400

    code update

--- essay_2/advertising data multiple lin reg code.R
+++ essay_2/advertising data multiple lin reg code.R
@@ -4,15 +4,44 @@ if (!requireNamespace("car", quietly = TRUE)) {
 
 install.packages("tidyverse")
 install.packages("caTools")
+install.packages("ggcorrplot")
+
+
 
 library(car)
 library(lmtest)
 library(tidyverse)
 library(caTools)
+library(ggcorrplot)
 
 advertising <- read.csv("Advertising.csv")
 str(advertising)
 head(advertising, 4)
+
+# Visualize variable distributions
+for (col_name in names(advertising)) {
+  if (is.numeric(advertising[[col_name]])) {
+    hist(advertising[[col_name]],
+         main = paste("Histogram of", col_name),
+         xlab = col_name)
+  }
+}
+
+
+# Find the outlier in newspaper
+Q1 <- quantile(advertising$newspaper, 0.25)
+Q3 <- quantile(advertising$newspaper, 0.75)
+IQR <- Q3 - Q1
+
+lower_bound <- Q1 - 1.5 * IQR
+upper_bound <- Q3 + 1.5 * IQR
+
+outlier_indices <- which(advertising$newspaper < lower_bound | advertising$newspaper > upper_bound)
+
+print(paste("Outlier value:", advertising$newspapers[outlier_indices]))
+print(paste("Outlier indices:", outlier_indices))
+advertising <- advertising[-outlier_indices, ]
+
 
 # Train test split
 set.seed(123)
@@ -41,3 +70,26 @@ par(mfrow = c(1, 1))  # Reset plot layout
 dwtest(model2)
 # sigma(model2)/mean(marketing$sales)
 
+#find MSE on test set
+predictions_model1 <- predict(model1, newdata = test_set)
+predictions_model2 <- predict(model2, newdata = test_set)
+
+mse_model1 <- mean((test_set$sales - predictions_model1)^2)
+print(paste("MSE for model1:", mse_model1))
+
+mse_model2 <- mean((test_set$sales - predictions_model2)^2)
+print(paste("MSE for model2:", mse_model2))
+
+
+#Cook's distance to remove influential points
+cooksD <- cooks.distance(model2)
+plot(cooksD,type="b",pch=18,col="red")
+influential <- cooksD[(cooksD > (3 * mean(cooksD, na.rm = TRUE)))]
+print("Indices of influential points:")
+print(names(influential))
+
+#correlation between predictors
+reduced_data <- subset(advertising, select=-X)
+corr_matrix = round(cor(reduced_data),2)
+
+ggcorrplot(corr_matrix, hc.order = FALSE, type="lower", lab=TRUE)
