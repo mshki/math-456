176383c0a7382a9ced00f600dce971b0823bc33c
mshki <132275360+mshki@users.noreply.github.com>
2025-03-31 23:37:59 -0400

    added analysis code

--- essay_3/glm.R
+++ essay_3/glm.R
@@ -3,6 +3,9 @@ library(caTools)
 library(tidyverse)
 library(caret)
 library(ggplot2)
+library(dplyr)
+library(pROC)
+library(ggfortify)
 theme_set(theme_bw())
 
 # Load data set
@@ -22,10 +25,63 @@ split <- sample.split(raisins$Class, SplitRatio = 0.8)
 train <- subset(raisins, split == TRUE)
 test <- subset(raisins, split == FALSE)
 
-# Logistic Regression Model
-mylogit <- glm(Class ~ Area + MajorAxisLength + MinorAxisLength + 
+# Model
+model <- glm(Class ~ Area + MajorAxisLength + MinorAxisLength + 
                  Eccentricity + ConvexArea + Extent + Perimeter, 
                data = train, 
                family = "binomial")
 
-summary(mylogit)
+summary(model)
+
+autoplot(model)
+
+# Predictions
+pred <- predict(model, test, type = "response")
+pred_class <- ifelse(pred > 0.5, "Besni", "Kecimen")
+
+# Confusion Matrix
+conf_matrix <- table(Predicted = pred_class, Actual = test$Class)
+print(conf_matrix)
+
+train.data <- train %>%
+  mutate(prob = predict(model, type = "response"))
+
+area_plot <- ggplot(train.data, aes(x = Area, y = Class)) +
+  geom_point(alpha = 0.3) +  
+  geom_smooth(method = "glm", method.args = list(family = "binomial")) +
+  labs(
+    title = "Logistic Regression Model for Area",
+    x = "Raisin Area",
+    y = "Probability of being Besni"
+  )
+
+print(area_plot)
+
+ecc_plot <- ggplot(train.data, aes(x = Eccentricity, y = Class)) +
+  geom_point(alpha = 0.3) +  
+  geom_smooth(method = "glm", method.args = list(family = "binomial")) +
+  labs(
+    title = "Logistic Regression Model for Eccentricity",
+    x = "Eccentricity",
+    y = "Probability of being Besni"
+  )
+
+print(ecc_plot)
+
+# ROC curve
+roc_curve <- roc(test$Class, pred)
+plot(roc_curve, col = "cadetblue", main = "ROC Curve")
+auc_value <- auc(roc_curve)
+cat("AUC:", auc_value, "\n")
+
+# Feature Importance
+importance <- summary(model)$coefficients[, "Estimate"]
+importance_df <- data.frame(Feature = names(importance), Estimate = importance)
+ggplot(importance_df, aes(x = reorder(Feature, Estimate), y = Estimate)) +
+  geom_bar(stat = "identity", fill = "pink") +
+  coord_flip() +
+  labs(title = "Feature Importance",
+       x = "Feature",
+       y = "Coefficient Estimate")
+
+
