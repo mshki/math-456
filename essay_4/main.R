ba7074d15d55f06d26a212c95313638bc447bfd9
samstevens127 <samuelstevens03@gmail.com>
2025-04-08 12:52:13 -0400

    added essay_4

---
+++ essay_4/main.R
@@ -0,0 +1,11 @@
+library("caret")
+library("rpart")
+library("rpart.plot")
+library("class")
+library("ggplot2")
+library("tree")
+
+
+
+data  <- read.csv("./data/2013-mb-dataset-Total-New-Zealand-Individual-Part-2.csv")
+
