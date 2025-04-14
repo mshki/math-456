a5e5ac7833dd4fb910748c36fd28e8991425df2e
samstevens127 <samuelstevens03@gmail.com>
2025-04-14 12:34:07 -0400

    changed data

--- essay_4/main.R
+++ essay_4/main.R
@@ -4,8 +4,21 @@ library("rpart.plot")
 library("class")
 library("ggplot2")
 library("tree")
+library("randomForest")
 
+# Load data set
+raisins <- read.csv("data/Raisin_Dataset.csv")
+raisins <- na.omit(raisins) # Remove missing values
 
+# Convert Class to binary (Besni = 1, Kecimen = 0)
+raisins$Class <- ifelse(raisins$Class == "Besni", 1, 0)
 
-data  <- read.csv("./data/2013-mb-dataset-Total-New-Zealand-Individual-Part-2.csv")
+# Shuffle data
+set.seed(456)
+train_idx  <- sample(1:nrow(raisins),size = 0.7 * nrow(raisins))
+train_data  <- raisins[train_idx,]
+test_data  <-  raisins[-train_idx,]
 
+tree_model  <-  tree(Class ~ .,data=train_data)
+
+summary(tree_model)
