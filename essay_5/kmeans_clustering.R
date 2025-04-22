5438c9f5eadbc53892c26d0bafcfbed12357ff55
mshki <132275360+mshki@users.noreply.github.com>
2025-04-22 19:39:11 -0400

    plots

--- essay_5/kmeans_clustering.R
+++ essay_5/kmeans_clustering.R
@@ -5,6 +5,7 @@ library(factoextra) # clustering algorithms & visualization
 library(ggplot2)
 library(dplyr)
 library(caTools)
+library("GGally")
 
 # Data Preparation
 data <- read.csv("data/SP500.csv")
@@ -15,28 +16,36 @@ df <- data[, -which(names(data) == "Date")]
 df <- df[, -which(names(df) == "Ticker")]
 
 ## Scale Values
-scaled <- scale(df)
+# scaled <- scale(df)
+
+## Split into Train and Test 
+set.seed(37)
+scaled <- df[sample(nrow(df)), ]
+head(scaled)
+split <- sample.split(scaled$Close, SplitRatio = 0.8)
+train <- subset(scaled, split == TRUE)
+test <- subset(scaled, split == FALSE)
 
 # Estimating Optimal Number of Clusters
 ## Elbow Method
-fviz_nbclust(scaled, kmeans, method="wss")
+fviz_nbclust(train, kmeans, method="wss")
 geom_vline(xintercept=3, linetype=2)
 
 ### we find that there isn't an elbow, so we get the silhouette instead
 ### in order to find the optimal number of clusters
 
 ## Average Silhouette
-fviz_nbclust(scaled, kmeans, method = "silhouette")
+fviz_nbclust(train, kmeans, method = "silhouette")
 ### this shows the optimal number of clusters as 2
 
 ## Gap Statistics
-gap_stat <- clusGap(scaled,
-                    FUN = kmeans,
-                    nstart = 25,
-                    K.max = 10,
-                    B = 50)
+# gap_stat <- clusGap(train,
+                    # FUN = kmeans,
+                    # nstart = 25,
+                    # K.max = 10,
+                    # B = 50)
 
-fviz_gap_stat(gap_stat)
+# fviz_gap_stat(gap_stat)
 ### calculating the gap statistics shows us
 ### the optimal number of clusters is 3
 
@@ -45,13 +54,13 @@ fviz_gap_stat(gap_stat)
 
 # K-Means Clustering
 set.seed(37)
-km.res <- kmeans(df, 3, nstart=25)
+km.res <- kmeans(train, 3, nstart=25)
 km.res
 
 ## mean of each feature
-aggregate(scaled, by=list(cluster=km.res$cluster), mean)
+aggregate(train, by=list(cluster=km.res$cluster), mean)
 
-dd <- cbind(scaled, cluster = km.res$cluster)
+dd <- cbind(train, cluster = km.res$cluster)
 head(dd)
 
 ## cluster stats for each observation
@@ -60,25 +69,27 @@ km.res$size # cluster size
 km.res$centers # cluster means
 
 # Visualize
-fviz_cluster(km.res, scaled[, -5],
+fviz_cluster(km.res, train[, -5],
              palette = "Set2", ggtheme = theme_minimal())
 
 # Historical vs. Predicted Closing Price Using Cluster-Based LM
 
 ## Calculate prev_close
-df$cluster <- km.res$cluster
+train$cluster <- km.res$cluster
 
-pred <- df %>%
+pred <- train %>%
   mutate(prev_close = lag(Close)) %>%
   na.omit()
 
-split <- sample.split(pred$cluster, SplitRatio = 0.8)
-train <- subset(pred, split == TRUE)
-test <- subset(pred, split == FALSE)
-
-model <- lm(Close ~ cluster + prev_close, data = train)
+model <- lm(Close ~ cluster + prev_close, data = pred)
 
 ## Predict next day's close
+km.res <- kmeans(test, 3, nstart=25)
+test$cluster <- km.res$cluster
+test <- test %>%
+  mutate(prev_close = lag(Close)) %>%
+  na.omit()
+
 test$predicted_close <- predict(model, newdata = test)
 
 ## Plot Actual v Predicted 
@@ -92,3 +103,12 @@ ggplot(test, aes(x = index)) +
   xlab("Day") +
   scale_color_manual(values = c("Actual" = "slateblue", "Predicted" = "pink")) +
   theme_minimal()
+
+# Diagonal Plots
+plot_data <- test[, 2:8]
+plot_data$Predicted <- test$predicted_close
+ggpairs(plot_data, mapping = aes(color = "pink"),
+        columns = 2:8,
+        title = "Diagonal Plot Colored by Predicted Class (kMeans)")
+        
+        
---
+++ essay_5/main.R
@@ -0,0 +1,103 @@
+library(dplyr)
+library(ggplot2)
+library(caTools)
+library(GGally)
+library(factoextra)
+
+df <- read.csv("data/SP500.csv")
+
+# Clean and prep
+df$Date <- as.Date(df$Date)
+df <- na.omit(df)
+
+# Create prev_close, prev_return feature
+df <- df %>%
+  group_by(Ticker) %>%
+  arrange(Date) %>%
+  mutate(prev_close = lag(Close),
+         prev_return = lag(Returns)) %>%
+  ungroup() %>%
+  na.omit()
+
+# Train/test split
+set.seed(77)
+split <- sample.split(df$Close, SplitRatio = 0.8)
+train <- subset(df, split == TRUE)
+test <- subset(df, split == FALSE)
+
+# KMeans Clustering
+## Select features for clustering
+cluster_features <- train %>%
+  select(Open, High, Low, Adjusted, Returns, Volume, prev_close)
+
+## Scale features
+scaled_train <- scale(cluster_features)
+
+# Estimating Optimal Number of Clusters
+## Elbow Method
+fviz_nbclust(scaled_train, kmeans, method="wss")
+geom_vline(xintercept=3, linetype=2)
+
+### we find that there isn't an elbow, so we get the silhouette instead
+### in order to find the optimal number of clusters
+
+## Average Silhouette
+fviz_nbclust(scaled_train, kmeans, method = "silhouette")
+### this shows the optimal number of clusters as 2
+
+## Gap Statistics
+gap_stat <- clusGap(scaled_train,
+                    FUN = kmeans,
+                    nstart = 25,
+                    K.max = 10,
+                    B = 50)
+
+fviz_gap_stat(gap_stat)
+### calculating the gap statistics shows us
+### the optimal number of clusters is 3
+
+### to be on the safer side, we take optimal 
+### number of clusters to be 3
+
+## Fit KMeans
+km.res <- kmeans(scaled_train, centers = 3)
+km.res
+
+train$cluster <- as.factor(km.res$cluster)
+
+test_scaled <- scale(test %>% select(Open, High, Low, Adjusted, Returns, Volume, prev_close),
+                     center = attr(scaled_train, "scaled:center"),
+                     scale = attr(scaled_train, "scaled:scale"))
+
+assign_cluster <- function(x, centers) {
+  dists <- apply(centers, 1, function(c) sum((x - c)^2))
+  return(which.min(dists))
+}
+
+test$cluster <- apply(test_scaled, 1, assign_cluster, centers = km.res$centers)
+test$cluster <- as.factor(test$cluster)
+
+# Predict next closing price using clusters 
+price_model <- lm(Close ~ cluster + prev_close, data = train)
+test$predicted_close <- predict(price_model, newdata = test)
+
+# Predict return using clusters 
+return_model <- lm(Returns ~ cluster + prev_return, data = train)
+test$predicted_return <- predict(return_model, newdata = test)
+
+# Plot: Actual vs Predicted Returns
+test$index <- 1:nrow(test)
+ggplot(test, aes(x = index)) +
+  geom_line(aes(y = Returns, color = "Actual Return")) +
+  geom_line(aes(y = predicted_return, color = "Predicted Return")) +
+  ggtitle("Actual vs Predicted Returns (Test Set)") +
+  ylab("Returns") +
+  xlab("Row Index") +
+  scale_color_manual(values = c("Actual Return" = "pink", "Predicted Return" = "slateblue")) +
+  theme_minimal()
+
+plot_data <- test[, 2:8]
+plot_data$Predicted <- test$predicted_return
+ggpairs(plot_data, mapping = aes(y = Predicted, color = "pink"),
+        columns = 2:8,
+        title = "Diagonal Plot Colored by Predicted Returns (kMeans)")
