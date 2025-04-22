cd0ea2f5e40dc5157062d9c88219e94201a6cbdd
mshki <132275360+mshki@users.noreply.github.com>
2025-04-22 09:32:13 -0400

    essay 5

---
+++ essay_5/kmeans_clustering.R
@@ -0,0 +1,7 @@
+# libraries
+library(tidyverse)  # data manipulation
+library(cluster)    # clustering algorithms
+library(factoextra) # clustering algorithms & visualization
+
+# data preparation
+df <- 
