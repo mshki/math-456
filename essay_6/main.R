cfbbae84fcd63e07b9e7fc396d43945f29dbe140
mshki <132275360+mshki@users.noreply.github.com>
2025-05-02 09:49:33 -0400

    finished code for essay_6

--- essay_6/main.R
+++ essay_6/main.R
@@ -50,14 +50,14 @@ adf.test(na.omit(data$Return))
 
 # 3.2 (Partial) Autocorrelation Function
 #      Checking whether Return is Autocorrelated
-acf(na.omit(data$Return), lag.max = 40, main='ACF of Return Values',col='pink')
-pacf(na.omit(data$Return), main='Partial Auto Correlation of Return Values',col='pink')
+acf(na.omit(data$Return), lag.max = 40, main='ACF of Return Values',col='violetred1')
+pacf(na.omit(data$Return), main='Partial Auto Correlation of Return Values',col='violetred1')
 # Observe: ACF shows Return series is not dependent on previous days error
 #          PACF shows Return series autocorrelates with lags 
 #          Thus, Return series are influenced by previous days Return
 
 # 3.3 Normality Test 
-ggplot(aes(Return), data=data) + geom_histogram(bins = 100,col='black',fill='pink') + ggtitle('Return of MSFt')
+ggplot(aes(Return), data=data) + geom_histogram(bins = 100,col='black',fill='violetred1') + ggtitle('Return of MSFt')
 skewness(data$Return); kurtosis(data$Return)
 # Observe: Returns are not normally distributed!
 #          Negative skew indicates Leptokurtic distribution
@@ -79,18 +79,118 @@ Box.test(na.omit(data$Return), type = "Ljung-Box")
 #          Thus Return series is not independent
 
 # 3.5 Autocorrelation of Absolute/Squaare Return
-a<- ggAcf(abs(na.omit(data$Return)), col='pink',main='Acf of Absolute Return of MSFT')
+a<- ggAcf(abs(na.omit(data$Return)), col='violetred1',main='Acf of Absolute Return of MSFT')
 p<- ggPacf(abs(na.omit(data$Return)),col='thistle1',main='PAcf of Absolute Return of MSFT')
 grid.arrange(a,p, ncol = 2, nrow = 1)
 # Observe: Absolute Return series show slow decay of auto correlation 
 
-c <- ggAcf(na.omit(data$Return)^2, lag.max = 40, col='red', main='ACF of squared Return Values')
-d<- ggPacf(na.omit(data$Return)^2,lag.max = 40, col='steelblue',main= 'PACF of squared Return Values')
+c <- ggAcf(na.omit(data$Return)^2, lag.max = 40, col='violetred1', main='ACF of squared Return Values')
+d<- ggPacf(na.omit(data$Return)^2,lag.max = 40, col='thistle1',main= 'PACF of squared Return Values')
 grid.arrange(c,d, ncol = 2, nrow = 1)
 # Observe: Squared Return series show slow decay of auto correlation 
 
 # 3.6 Volatility Clustering 
 chart.RollingPerformance(na.omit(data$Return),width = 22,FUN = 'sd.annualized',scale=252, main = 'Rolling 1 month Volatility')
+# Observe: We can significantly improve our ARMA model using GARCH 
 
 # 4 Fit the GARCH Model
+
+#      Model 1: Fit ARMA(0,0)-gjrGARCH(1,1) model with Student t-distribution
+CAT_garch_1 <- ugarchspec(mean.model =
+                            list(armaOrder=c(0,0)),variance.model = list(model = 'eGARCH', garchOrder = c(1, 1)),distribution = 'std')
+fit_garch_1 <- ugarchfit(spec = CAT_garch_1, data= na.omit(data$Return))
+aic_1 <- infocriteria(fit_garch_1)[1]
+fit_garch_1
+#plot(fit_garch_1,which='all')
+
+#      Model 2: Fit ARMA(1,1)-gjrGARCH(1,1) model with Student t-distribution
+CAT_garch_2 <- ugarchspec(mean.model = list(armaOrder=c(1,1)),variance.model = list(model = 'eGARCH', garchOrder = c(1, 1)),distribution = 'std')
+fit_garch_2 <- ugarchfit(spec = CAT_garch_2, data= na.omit(data$Return))
+aic_2 <- infocriteria(fit_garch_2)[1]
+aic_2
+fit_garch_2
+#plot(fit_garch_2,which='all')
+
+#      Model 3: Fit ARMA(2,2)-gjrGARCH(1,1) model with Student t-distribution
+CAT_garch_3 <- ugarchspec(mean.model = list(armaOrder=c(2,2)),variance.model = list(model = 'eGARCH', garchOrder = c(1, 1)),distribution = 'std')
+fit_garch_3 <- ugarchfit(spec = CAT_garch_3, data= na.omit(data$Return))
+aic_3 <- infocriteria(fit_garch_3)[1]
+fit_garch_3
+#plot(fit_garch_3,which='all')
+
+#      Model 4: Fit ARMA(0,0)-eGARCH(1,2) model with Student t-distribution
+CAT_garch_4 <- ugarchspec(mean.model = list(armaOrder=c(0,0)),variance.model = list(model = 'eGARCH', garchOrder = c(1, 2)),distribution = 'std')
+fit_garch_4 <- ugarchfit(spec = CAT_garch_4, data= na.omit(data$Return))
+aic_4 <- infocriteria(fit_garch_4)[1]
+fit_garch_4
+#plot(fit_garch_4,which='all')
+
+#      Model 5: Fit ARMA(1,1)-eGARCH(2,1) model with Student t-distribution
+CAT_garch_5 <- ugarchspec(mean.model = list(armaOrder=c(1,1)),variance.model = list(model = 'eGARCH', garchOrder = c(2,1)),distribution = 'std')
+fit_garch_5 <- ugarchfit(spec = CAT_garch_5, data= na.omit(data$Return))
+aic_5 <- infocriteria(fit_garch_5)[1]
+fit_garch_5
+#plot(fit_garch_5,which='all')
+
+#      Model 6: Fit ARMA(3,1)-eGARCH(1,1) model with Student t-distribution
+CAT_garch_6 <- ugarchspec(mean.model = list(armaOrder=c(3,1)),variance.model = list(model = 'eGARCH', garchOrder = c(1, 1)),distribution = 'std')
+fit_garch_6 <- ugarchfit(spec = CAT_garch_6, data= na.omit(data$Return))
+aic_6 <- infocriteria(fit_garch_6)[1]
+fit_garch_6
+#plot(fit_garch_6,which='all')
+
+#      Model 7: Fit ARMA(3,2)-eGARCH(1,1) model with Student t-distribution
+CAT_garch_7 <- ugarchspec(mean.model = list(armaOrder=c(3,2)),variance.model = list(model = 'eGARCH', garchOrder = c(1, 1)),distribution = 'std')
+fit_garch_7 <- ugarchfit(spec = CAT_garch_7, data= na.omit(data$Return))
+aic_7 <- infocriteria(fit_garch_7)[1]
+fit_garch_7
+#plot(fit_garch_7,which='all')
+
+#      Model 8: Fit ARMA(1,3)-eGARCH(1,1) model with Student t-distribution
+CAT_garch_8 <- ugarchspec(mean.model = list(armaOrder=c(1,3)),variance.model = list(model = 'eGARCH', garchOrder = c(1, 1)),distribution = 'std')
+fit_garch_8 <- ugarchfit(spec = CAT_garch_8, data= na.omit(data$Return))
+aic_8 <- infocriteria(fit_garch_8)[1]
+fit_garch_8
+#plot(fit_garch_8,which='all')
+
+
+# 5 Model Selection
+Model = c('fit_garch_1','fit_garch_2','fit_garch_3','fit_garch_4','fit_garch_5','fit_
+garch_6','fit_garch_7','fit_garch_8')
+AIC = c(aic_1, aic_2, aic_3, aic_4, aic_5, aic_6, aic_7, aic_8)
+(model <- data.frame(Model,AIC))
+which.min(model[,'AIC'])
+# Observe: The output was 5, so our selected model is fit_garch_5
+
+# 6 Analyze Selected Model 
+#      Statistics & Plot
+fit_garch_5
+plot(fit_garch_5,which='all')   
+
+#      Persistence of Volatility
+persistence(fit_garch_5) 
+
+#      Convergence of the Model
+print(convergence(fit_garch_5))
+
+# 7 Forecasting (50 days ahead)
+forecast_1 <-ugarchforecast(fit_garch_5,data=data,n.ahead=50)
+forecast_1
+
+#      Rolling Forecast 
+fit_roll <- ugarchfit(CAT_garch_5, data= na.omit(data$Return),out.sample =500)
+fore_roll <- ugarchforecast(fit_roll, n.ahead=50, n.roll=50)
+fore_roll
+
+par(mfrow=c(1,2))
+plot(fore_roll,which=1)
+plot(fore_roll,which=2)
+
+#      Bootstrap Forecast
+par(mfrow=c(1,2))
+fore_boot <- ugarchboot(fit_garch_5,data = na.omit(data$Return), method = c("Partial", "Full")[1], n.ahead = 50, n.bootpred = 500)
+plot(fore_boot,which=2)
+plot(fore_boot,which=3)
+head(sigma(forecast_1))
+
 
